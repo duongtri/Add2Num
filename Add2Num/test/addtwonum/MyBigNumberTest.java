e9ca911e9b67f9592b7d02b62929e4b9e3bf4f54
ltduy24101997 <43946897+ltduy24101997@users.noreply.github.com>
2018-12-23 00:29:51 +0700

    Update MyBigNumberTest.java

--- Add2Num/test/addtwonum/MyBigNumberTest.java
+++ Add2Num/test/addtwonum/MyBigNumberTest.java
@@ -25,34 +25,26 @@ public class MyBigNumberTest implements IReceiver {
      */
     @Test
     public void testSum1() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "10";
-            String str2 = "10";
-            String expResult = "20";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.printf(e);
-        }
+        System.out.printf("sum :");
+        String str1 = "10";
+        String str2 = "10";
+        String expResult = "20";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testSum2() {
-        try {
-            System.out.printf("sum :");
-            String str1 = "99";
-            String str2 = "99";
-            String expResult = "198";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.printf(e);
-        }
+        System.out.printf("sum :");
+        String str1 = "99";
+        String str2 = "99";
+        String expResult = "198";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
     }
 
     @Test
@@ -73,11 +65,23 @@ public class MyBigNumberTest implements IReceiver {
 
     @Test
     public void testSum4() {
+        System.out.printf("sum :");
+        String str1 = "99";
+        String str2 = "999";
+        String expResult = "1098";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSum5() {
         try {
             System.out.printf("sum :");
-            String str1 = "99";
-            String str2 = "999";
-            String expResult = "1098";
+            String str1 = "21";
+            String str2 = "10)";
+            String expResult = "31";
             MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             String result = mybignumber.sum(str1, str2);
@@ -86,12 +90,12 @@ public class MyBigNumberTest implements IReceiver {
             System.out.printf(e);
         }
     }
-
-    @Test
-    public void testSum5() {
+    
+     @Test
+    public void testSum6() {
         try {
             System.out.printf("sum :");
-            String str1 = "21";
+            String str1 = "";
             String str2 = "10)";
             String expResult = "31";
             MyBigNumberTest test = new MyBigNumberTest();
