601977de7b1d5f4d689a84339164d0fe2ed136cd
ltduy24101997 <43946897+ltduy24101997@users.noreply.github.com>
2018-12-23 12:40:08 +0700

    Update MyBigNumberTest.java

--- Add2Num/test/addtwonum/MyBigNumberTest.java
+++ Add2Num/test/addtwonum/MyBigNumberTest.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package addtwonum;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
 import addtwonum.NumberException;
 import addtwonum.IReceiver;
 import addtwonum.MyBigNumber;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -17,109 +10,88 @@ import addtwonum.MyBigNumber;
  */
 public class MyBigNumberTest implements IReceiver {
 
+    public static int i = 1;
+
     public MyBigNumberTest() {
     }
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
 
     /**
      * Test of sum method, of class MyBigNumber.
      */
     @Test
     public void testSum1() {
-        try {
-            System.out.println("sum :");
-            String str1 = "10";
-            String str2 = "10";
-            String expResult = "20";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+        String str1 = "10";
+        String str2 = "10";
+        String expResult = "20";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testSum2() {
-        try {
-            System.out.println("sum :");
-            String str1 = "99";
-            String str2 = "99";
-            String expResult = "198";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+        String str1 = "99";
+        String str2 = "99";
+        String expResult = "198";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testSum3() {
-        try {
-            System.out.println("sum :");
-            String str1 = "999";
-            String str2 = "99";
-            String expResult = "1098";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+        String str1 = "999";
+        String str2 = "99";
+        String expResult = "1098";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
     }
 
     @Test
     public void testSum4() {
-        try {
-            System.out.println("sum :");
-            String str1 = "99";
-            String str2 = "999";
-            String expResult = "1098";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-            assertEquals(expResult, result);
-        } catch (NumberFormatException e) {
-            System.out.println(e);
-        }
+        String str1 = "99";
+        String str2 = "999";
+        String expResult = "1098";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
+        assertEquals(expResult, result);
+
     }
 
     @Test
     public void testSum5() {
-        String expResult = "3";
-        try {
-            System.out.println("sum :");
-            String str1 = "21";
-            String str2 = "10)";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result = mybignumber.sum(str1, str2);
-        } catch (NumberFormatException e) {
-           assertEquals(expResult, e.toString());
-        }
+        expected.expect(NumberException.class);
+        expected.expectMessage("3");
+        String str1 = "21-";
+        String str2 = "10";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
     }
-    
+
     @Test
     public void testSum6() {
-        String expResult = "1";
-        try {
-            System.out.println("sum :");
-            String str1 = "21";
-            String str2 = "";
-            MyBigNumberTest test = new MyBigNumberTest();
-            MyBigNumber mybignumber = new MyBigNumber(test);
-            String result =mybignumber.sum(str1, str2);
-        } catch (NumberFormatException e) {
-            assertEquals(expResult,e.toString());
-        }
+        expected.expect(NumberException.class);
+        expected.expectMessage("1");
+        String str1 = "21";
+        String str2 = "";
+        MyBigNumberTest test = new MyBigNumberTest();
+        MyBigNumber mybignumber = new MyBigNumber(test);
+        String result = mybignumber.sum(str1, str2);
     }
 
     @Override
     public void send(String msg) {
-        System.out.println(msg + "\n");
+        System.out.println("Case" + i + ":");
+        i++;
     }
 
 }
